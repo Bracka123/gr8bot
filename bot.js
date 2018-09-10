0635abb919cb2fbc045c45bb646f1ceb13d76c6f
Bracka123 <42469081+Bracka123@users.noreply.github.com>
2018-09-10 21:17:26 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+
+
+bot.on('message', message => {
+
+    
+    var msg = message.content.toLowerCase();
+    var sender = message.author;
+    var prefix = "g"
+   
+      
+    if (msg === prefix + 'hi') {
+        message.channel.send('Hello')
+    }
+    
+    
+
+
+
+   
+   
+});
+
+bot.on('ready', () => {
+
+    console.log("Hello guys")
+    bot.user.setActivity(`Nothing & With ${bot.guilds.size} Server`)
+});
+
+bot.login(process.env.BOT_TOKEN);
