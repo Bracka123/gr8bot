374a1d521129cd2aa02bfd25d7bbb15a03311a43
Bracka123 <42469081+Bracka123@users.noreply.github.com>
2018-11-10 13:11:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ bot.on('message', message => {
     
   var sender = message.author;
     
-  var prefix = "g"
+  var prefix = "!";
    
       
     
@@ -33,37 +33,7 @@ bot.on('ready', () => {
     
   console.log("Hello guys")
     
-  bot.user.setActivity(`Nothing & With ${bot.guilds.size} Server`)
+  
 });
-
-bot.on('guildMemberAdd', member => {
-    
-    
-  if (member.guild.id !== serverStats.guildID) return;
-
-    
-    
-  bot.channels.get(serverStats.totalUsersID).setName(`Total Users : ${member.guild.memberCount}`)
-    
-  bot.channels.get(serverStats.memberCountID).setName(`Member Count : ${member.guild.members.filter(m => !m.user.bot).size}`);
-    
-  bot.channels.get(serverStats.botCountID).setName(`Bot Count : ${member.guild.members.filter(m => m.user.bot).size}`);
-
-
-});
-
-bot.on('guildMemberRemove', member => {
-    
-  if (member.guild.id !== serverStats.guildID) return;
-
-   
-  bot.channels.get(serverStats.totalUsersID).setName(`Total Users : ${member.guild.memberCount}`)
-    
-  bot.channels.get(serverStats.memberCountID).setName(`Member Count : ${member.guild.members.filter(m => !m.user.bot).size}`);
-    
-  bot.channels.get(serverStats.botCountID).setName(`Bot Count : ${member.guild.members.filter(m => m.user.bot).size}`);
-
-});
-
 
 bot.login(process.env.BOT_TOKEN);
